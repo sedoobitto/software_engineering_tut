df509572371f8614cb7d6504ee8b8589207ed5fd
Sedoo Bitto <sedoobitto@gmail.com>
2023-02-26 07:37:40 -0800

    Comment out code casusing infinite loop

---
+++ low_level_programming/0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - This program causes an infitte loop, comment a fix.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incomming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)
+	{
+		putchar(i);
+	}*/
+	 
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
