500724e2270f707b50396bd506bbbfa607011f1d
Sedoo Bitto <sedoobitto@gmail.com>
2023-03-01 20:15:51 -0800

    Update pointer value to 98

---
+++ low_level_programming/0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter
+ * and updates the value it points to 98.
+ * @n: pointer.
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
