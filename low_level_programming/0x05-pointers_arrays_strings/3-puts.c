afff42eca245a7cca544e2028f69e8939e4a9729
Sedoo Bitto <sedoobitto@gmail.com>
2023-03-01 20:17:41 -0800

    Print a string

---
+++ low_level_programming/0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string, followed by a new line, to stdout.
+ * @str: string.
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+
+	for ( ; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+
+	_putchar('\n');
+}
