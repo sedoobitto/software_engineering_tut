0ec88299039269c0954f23b4e1a4f27fc95eed09
Sedoo Bitto <sedoobitto@gmail.com>
2023-03-02 08:51:09 -0800

    Update Header file

---
+++ low_level_programming/0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _putchar(char c);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
