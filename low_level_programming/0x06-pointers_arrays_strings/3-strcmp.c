1c233751c04ce941239598fbe1afea2c9ab10497
Sedoo Bitto <sedoobitto@gmail.com>
2023-03-04 12:31:35 -0800

    Compare two strings

---
+++ low_level_programming/0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: num.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	len1 = strlen
+}
