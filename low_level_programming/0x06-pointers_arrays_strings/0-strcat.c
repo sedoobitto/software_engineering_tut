637867d21b7327fc0c812092d5922f0b88b0c298
Crucifix Bee <crucifixbee@yahoo.com>
2023-03-05 20:42:17 +0100

    Concatenate two strings

--- low_level_programming/0x06-pointers_arrays_strings/0-strcat.c
+++ low_level_programming/0x06-pointers_arrays_strings/0-strcat.c
@@ -4,23 +4,29 @@
  * _strcat - Appends the scr string to the dest string,
  * overwritting the terminating null byte at the end of dest,
  * and adds a terminating null byte.
- * @dest: string 1
- * @src: string 2
+ * @dest: destination string 
+ * @src: source string 
  *
  * Return: dest.
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i, len1, len2;
-
-	len1 = strlen(dest);
-	len2 = strlen(src);
-
-	for (i = 0; i <= len2; i++)
+	int i = 0;
+	while (dest[i] != '\0')
 	{
-		dest[len1 + i] = src[i];
+		i += 1;
 	}
+
+	int j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
 
 	return (dest);
 }
