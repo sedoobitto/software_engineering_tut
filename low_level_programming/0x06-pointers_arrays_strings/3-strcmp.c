c159c1ba21a4400a9e9197488ab86b15fbbaeb27
Crucifix Bee <crucifixbee@yahoo.com>
2023-03-06 10:29:59 +0100

    Compare two strings

--- low_level_programming/0x06-pointers_arrays_strings/3-strcmp.c
+++ low_level_programming/0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,5 +10,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	len1 = strlen
+	int i = 0;
+	int j = 0;
+
+	while (j == 0)
+	{
+		if((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		j = (*(s1 + i)) - (*(s2 + i));
+		i++;
+	}
+	return (j);
 }
